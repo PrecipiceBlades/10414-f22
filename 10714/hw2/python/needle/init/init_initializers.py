7da6e27d97d97435a1ce8b9d16894af7bce2cdd6
Ruitao Yi <ryi@zoox.com>
2025-11-12 03:10:43 +0000

    Convert hw2 and hw3 from submodules to regular directories

---
+++ 10714/hw2/python/needle/init/init_initializers.py
@@ -0,0 +1,23 @@
+import math
+from .init_basic import *
+from typing import Any
+
+def xavier_uniform(fan_in: int, fan_out: int, gain: float = 1.0, **kwargs: Any) -> "Tensor":
+    a = gain * math.sqrt(6.0 / (fan_in + fan_out))
+    return rand(fan_in, fan_out, low=-a, high=a, **kwargs)
+
+def xavier_normal(fan_in: int, fan_out: int, gain: float = 1.0, **kwargs: Any) -> "Tensor":
+    std = gain * math.sqrt(2.0 / (fan_in + fan_out))
+    return randn(fan_in, fan_out, mean=0.0, std=std, **kwargs)
+
+def kaiming_uniform(fan_in: int, fan_out: int, nonlinearity: str = "relu", **kwargs: Any) -> "Tensor":
+    assert nonlinearity == "relu", "Only relu supported currently"
+    gain = math.sqrt(2.0)
+    bound = gain * math.sqrt(3.0 / fan_in)
+    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
+
+def kaiming_normal(fan_in: int, fan_out: int, nonlinearity: str = "relu", **kwargs: Any) -> "Tensor":
+    assert nonlinearity == "relu", "Only relu supported currently"
+    gain = math.sqrt(2.0)
+    std = gain / math.sqrt(fan_in)
+    return randn(fan_in, fan_out, mean=0.0, std=std, **kwargs)
